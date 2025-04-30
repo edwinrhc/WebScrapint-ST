170ab50456a69fbe8a221df746dc72ce17a67d11
Edwin HC <edwinrhc@gmail.com>
2025-04-30 17:28:56 -0500

    feat: Actualización de búsqueda

--- Sifonavi/scrape_data.py
+++ Sifonavi/scrape_data.py
@@ -17,22 +17,23 @@ if response.status_code == 200:
 
     enlaces = soup.find_all('a')
 
+    variable = "Res. U. A. N"
+    variable_norm = variable.casefold()
     # Abrimos archivo CSV
     with open("resoluciones.csv", "w", newline='', encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile)
+        writer = csv.writer(csvfile, delimiter="|",quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(["Nombre del Enlace", "URL"])  # encabezado
 
         for enlace in enlaces:
             texto = enlace.get_text(strip=True)
-            if texto.lower().startswith("resolución secretarial"):
+            if texto.lower().startswith(variable_norm):
                 href = enlace.get('href')
-
                 # Escribimos fila en CSV
                 writer.writerow([texto, href])
 
                 print(f"{texto} - {href}")
 
-    print("\n✅ Enlaces guardados correctamente en 'resoluciones.csv'")
-    time.sleep(random.uniform(2, 5))
+    print("\n✅ Enlaces guardados correctamente en 'enlaces.csv'")
+    time.sleep(random.uniform(2, 6))
 else:
     print(f"❌ Error al acceder a la página: {response.status_code}")
